f4fbfe0b1fc898549fb6ca2082f379e951cc7109
Dawit Tafesse <Dawit@Dawits-MacBook-Pro.local>
2018-10-06 19:12:33 -0500

    test script

--- spotPlaylist/tests/unit/example.spec.js
+++ spotPlaylist/tests/unit/example.spec.js
@@ -1,13 +1,12 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+import App from '@/App'; //  same as '@/' == '../../src'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
-  })
-})
+describe('App.vue', () => {
+  it('should render correct contents', () => {
+    const wrapper = shallowMount(Counter);
+    wrapper.find('button').trigger('click');
+    wrapper.find('button').trigger('click');
+    expect(wrapper.text()).to.include(2);
+  });
+});
