0d461b4248b6c9e28b2b4ffa0b56c52f633e57b9
Dawit Tafesse <Dawit@Dawits-MacBook-Pro.local>
2018-10-14 15:15:12 -0500

    loggin and signup form have been updated

--- spotPlaylist/src/components/User/signUp.vue
+++ spotPlaylist/src/components/User/signUp.vue
@@ -1,24 +1,26 @@
 <template>
-    <div class="container">
-        <div class="signupForm">
-            <form>
-                <h2>Sign Up</h2>
-                <div class="form-group">
-                    <input type="email" class="form-control" name="email" placeholder="Email Address" required="required">
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" name="password" placeholder="Password" required="required">
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required">
-                </div>        
-                <div class="form-group">
-                    <button type="submit" class="btn btn-outline-primary btn-lg">Sign Up</button>
-                </div>
-            </form>
-        </div>
-   
-    </div>
+    <v-container fluid>
+        <v-layout align-center justify-center row fill-height >
+            <v-flex xs12 sm8 md4>
+                <v-card class="elevation-12">
+                    <v-toolbar dark color="cyan">
+                        <v-toolbar-title>Sign Up</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form>
+                            <v-text-field prepend-icon="person" name="email" label="Email" type="email" color="cyan"></v-text-field>
+                            <v-text-field prepend-icon="lock" name="password" label="Password" id="password" type="password" color="cyan"></v-text-field>
+                            <v-text-field prepend-icon="lock" name="confirmPassword" label="Confirm Password" id="confirmPassword" type="password" color="cyan"></v-text-field>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="cyan">Sumbit</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
@@ -28,47 +30,7 @@ export default {
 </script>
 
 <style scoped>
-	.signupForm {
-		width: 425px;
-		margin: 0 auto;
-		padding: 30px 0;
-	}
-    form {
-    	margin-bottom: 15px;
-        background: #fff;
-		border: 1px solid #f4f4f4;
-        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-        padding: 40px 50px;
-    }
-	.form-control {
-        min-height: 41px;
-		box-shadow: none;
-		border-color: #e6e6e6;
-	}
-	.form-control:focus {
-		/* border-color: #00c1c0; */
-        border-color: rgb(21,127,251);
-	}
-	h2 {
-		color: #333;
-		font-weight: bold;
-        margin: 0 0 25px;
-    }
 
-	.form-group {
-		margin-bottom: 20px;
-	}
-	label {
-		font-weight: normal;
-		font-size: 13px;
-	}
-    .btn {        
-        font-size: 16px;
-        font-weight: bold;
-		min-width: 140px;
-        outline: none !important;
-        border-radius: 3px;
-    }	
 </style>
 
 
